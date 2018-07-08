d8e9afe6ef061fe5761b5aa9872a9ba8c2fd64db
Liedig <sliedig@amazon.com>
2018-07-09 00:20:04 +0800

    Added build scripts and updated template

--- RegisterIncidentTask/Function.cs
+++ RegisterIncidentTask/Function.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
